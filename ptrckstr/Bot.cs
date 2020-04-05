184af2a96f19c0b66525faa40149fe6649c07a0c
Allvaa <alvamailindo@gmail.com>
2020-04-05 20:52:23 +0700

    masi nub

---
+++ ptrckstr/Bot.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using System;
+using System.Threading.Tasks;
+
+namespace ptrckstr
+{
+    class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public async Task TaskAsync()
+        {
+            Client = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "kowakoawkoawkoawko",
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            });
+
+            Client.Ready += OnClientReady;
+            Client.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().Equals("haee"))
+                await e.Message.RespondAsync("Hi");
+            };
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            Console.WriteLine($"ppppppppppppppppppppppppppppppppppppp {e.Client.CurrentUser.Username}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ptrckstr/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ptrckstr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.TaskAsync().GetAwaiter().GetResult();
+        }
+    }
+}
