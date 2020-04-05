b05394c66d953e6230a9d87abf9a3db4e0f88c6b
Allvaa <alvamailindo@gmail.com>
2020-04-05 22:17:17 +0700

    config

--- ptrckstr/Bot.cs
+++ ptrckstr/Bot.cs
@@ -1,6 +1,8 @@
 ﻿using DSharpPlus;
 using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace ptrckstr
@@ -8,11 +10,13 @@ namespace ptrckstr
     class Bot
     {
         public DiscordClient Client { get; private set; }
+
         public async Task TaskAsync()
         {
+            Config Configuration = await LoadConfigAsync();
             Client = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "kowakoawkoawkoawko",
+                Token = Configuration.Token,
                 TokenType = TokenType.Bot,
                 AutoReconnect = true,
                 LogLevel = LogLevel.Debug,
@@ -34,6 +38,15 @@ namespace ptrckstr
         {
             Console.WriteLine($"ppppppppppppppppppppppppppppppppppppp {e.Client.CurrentUser.Username}");
             return Task.CompletedTask;
+        }
+
+        public async Task<Config> LoadConfigAsync()
+        {
+            using (StreamReader r = new StreamReader("config.json"))
+            {
+                string json = await r.ReadToEndAsync().ConfigureAwait(false);
+                return JsonConvert.DeserializeObject<Config>(json);
+            }
         }
     }
 }
---
+++ ptrckstr/Config.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ptrckstr
+{
+    public struct Config
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+    }
+}
